f81e222a7f9ff5d9c3ad3ca3fd152a837eed32ed
tiesco-assis <francisco.sts@hotmail.com>
2020-02-05 20:36:11 -0300

    Implementando a css

--- index.php
+++ index.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <title>Shopping Cart System (Study)</title>
+
+    <link rel="stylesheet" href="css/bootcss.css">
+    
 </head>
 <body>
-    
+    <section>
+        <h1>Escolha os cursos que deseja comprar</h1>
+
+
+    </section>
 </body>
 </html>
