0ae22c5d782f19171033fb4edc596763a2d859c0
tiesco-assis <francisco.sts@hotmail.com>
2020-02-05 21:12:28 -0300

    Criando area do topo com logo

--- index.php
+++ index.php
@@ -1,18 +1,31 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shopping Cart System (Study)</title>
 
     <link rel="stylesheet" href="css/bootcss.css">
-    
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/icons-fontawesome/css/all.css">
+
 </head>
+
 <body>
-    <section>
-        <h1>Escolha os cursos que deseja comprar</h1>
+    <header class="container">
+        <div class="content-95-900">
+            <i class="fab fa-connectdevelop logo"></i>
+            <h1>Escolha os cursos que deseja comprar</h1>
+            <div class="clear"></div>
+        </div>
+    </header>
 
 
-    </section>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="css/icons-fontawesome/js/all.js"></script>
+
 </body>
+
 </html>
